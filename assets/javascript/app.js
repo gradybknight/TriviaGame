b4c07d9ef6939ee6b5f8bcbbcc17b1299dadb340
GRADY KNIGHT <gradyknight@gradys-mbp.lan>
2018-02-19 22:09:40 -0500

    mostly done logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,17 @@
-// The below code fills in the first row of the table
 var queryURL = "https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple";
-var randomAnswer
-var isAjaxDone=false;
 var correctAnswerScreenIndex=0;
 var arrayOfDivsToPutAnswersIn=[1,2,3,4];
+var questionTimer;
+var gameTimer;
+var questionTimeRemaining = 15*1000;
+var gameTimeRemaining = 10*10*1000;
+var countCorrect = 0;
+var countWrong =0;
+var questionIndex = 0;
 
-function populateQuestionAndAnswers(triviaObject){
+function populateQuestionAndAnswers(triviaObject,questionIndex){
     $('#questionDiv').html(triviaObject.question);
     correctAnswerScreenIndex=Math.floor(Math.random()*4)+1;
-    // correctAnswerScreenIndex==correctAnswerScreenIndex.toString();
     $("#"+correctAnswerScreenIndex).html(triviaObject.correct_answer);
     arrayOfDivsToPutAnswersIn=removeValueFromArray(correctAnswerScreenIndex,arrayOfDivsToPutAnswersIn);
     for (var i=0;i<arrayOfDivsToPutAnswersIn.length;i++){
@@ -28,33 +31,77 @@ function removeValueFromArray(theValue,theArray){
     return newArray;
 }
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-    isAjaxDone=true;
-    console.log(response.results[0]);
-    var theResponse=response.results[Math.floor(Math.random()*response.results.length)];
-    populateQuestionAndAnswers(theResponse);
-    // // Obtain a reference to the tbody element in the DOM
-    // var parentDiv = $('tbody');
-    // console.log(parentDiv);
-    // // Create and save a reference to new empty table row
-    // var newDiv = $("<tr>");
-    // console.log(newDiv);
-    // // Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
-    // var theKeys=["Title","Year","Actors"];
-    // for (var i=0;i<theKeys.length;i++){
-    // var newTD = $("<td>");
-    // // var theString = response.theKeys[i];
-    // newTD.text(response[theKeys[i]]);
-    // console.log(newTD);
-    // newDiv.append(newTD);
-    // };
-    // parentDiv.append(newDiv);
-    // // Append the td elements to the new table row
-    // // Append the table row to the tbody element
-});
+function setLevel(){
+    if (this.id!=="easy"){
+        queryURL = "https://opentdb.com/api.php?amount=10&category=27&difficulty="+ this.id +"&type=multiple";
+    } else {
+        //stupid API sets default to easy and doesn't accept difficulty=easy ?!?!?
+        queryURL = "https://opentdb.com/api.php?amount=10&category=27&type=multiple"
+    };
+    hideTheHeader();
+    getTheQuestions();
+    
+}
+function hideTheHeader(){
+    $("#theHeaderDiv").hide();
+}
 
+function showTheHeader(){
+    $("#theHeaderDiv").show();
+}
+
+function getTheQuestions(){
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        console.log(response.results[0]);
+        var theResponse=response.results[Math.floor(Math.random()*response.results.length)];
+        populateQuestionAndAnswers(theResponse,questionIndex);
+    });
+};
+
+function startQuestionTimer(){
+    questionTimer = setInterval(function(){ countDownTimer() },1000);
+}
+
+function countDownTimer(){
+    questionTimeRemaining=-1000;
+    if (questionTimeRemaining<=0) {
+        countwrong++;
+        questionIndex++;
+        clearInterval(questionTimer);
+        populateQuestionAndAnswers(triviaObject,questionIndex);
+        startQuestionTimer();
+    }
+
+}
+
+function endTheGame(){
+    console.log("the game is over");
+}
+
+function startGameTimer(){
+    gameTimer = setInterval(function(){ gameTimer() },1000);
+}
+
+function gameTimer(){
+    gameTimeRemaining=-1000;
+    if (gameTimeRemaining<=0) {
+        countwrong++;
+        clearInterval(gameTimer);
+
+    }
+
+}
+
+function alertFunction(){
+    console.log(Date.now());
+}
+function testInterval(){
+    var intervalTester = window.setInterval(alertFunction,2000);
+}
+// testInterval();
 console.log("i'm waiting");
+$(document).on("click",".levelChoice",setLevel);
